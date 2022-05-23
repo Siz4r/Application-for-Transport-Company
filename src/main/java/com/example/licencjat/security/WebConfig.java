6f7a95bab75c23983c02c1e3bcf2cb1acb150c57
Siz4r <zaq134215@wp.pl>
2022-05-23 18:23:28 +0200

    To refactor

--- src/main/java/com/example/licencjat/security/WebConfig.java
+++ src/main/java/com/example/licencjat/security/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowCredentials(true)
-                .allowedOrigins("http://localhost:3000", "http://localhost:3455")
+                .allowedOrigins("http://localhost:3000", "http://localhost:7627")
                 .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
     }
 }
