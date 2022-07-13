6ee61976a7a28364d42ebe2298c07ae13b6bb435
Siz4r <zaq134215@wp.pl>
2022-07-13 14:22:53 +0200

    Refactor adding messages

--- src/main/java/com/example/licencjat/messages/models/Message.java
+++ src/main/java/com/example/licencjat/messages/models/Message.java
@@ -1,10 +1,7 @@
 package com.example.licencjat.messages.models;
 
 import com.example.licencjat.conversations.models.Conversation;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -15,6 +12,7 @@ import java.util.UUID;
 
 @Entity
 @Getter @Setter
+@Builder
 @AllArgsConstructor @NoArgsConstructor
 public class Message {
     @Id
