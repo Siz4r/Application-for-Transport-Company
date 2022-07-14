6cc0f3c3fb48c6a2f4aa642eb4d4a3ce2db7721c
Siz4r <zaq134215@wp.pl>
2022-07-14 17:07:11 +0200

    Init

---
+++ src/test/java/com/example/licencjat/user/UserDataValidatorTest.java
@@ -0,0 +1,16 @@
+package com.example.licencjat.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDataValidatorTest {
+
+    @Test
+    void validateUserWebInput() {
+    }
+
+    @Test
+    void validateUserUpdateCommand() {
+    }
+}
